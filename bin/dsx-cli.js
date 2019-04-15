6efc03010121b8a1d85a2ce691cae45cd7107662
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-04-15 08:30:32 -0700

    Use image.dsx.js instead of dockerfile.dsx.js.
    Reviewed by @tolmasky.

--- bin/dsx-cli.js
+++ bin/dsx-cli.js
@@ -5,7 +5,7 @@ const options = require("commander")
     .version(require("../package").version)
     .parse(process.argv);
 
-const relative = options.args[0] || "dockerfile.dsx.js";
+const relative = options.args[0] || "image.dsx.js";
 const filename = resolve(process.cwd(), relative);
 
 require("../build")({ filename });

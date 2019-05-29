bc0f4134ffaec38af1d9ac0a21b349895df61d0e
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-05-29 11:18:15 -0700

    Add scope finder.
    Reviewed by @tolmasky.

---
+++ to-lambda-form/map-accum-node.js
@@ -0,0 +1,50 @@
+const { isArray } = Array;
+const t = require("@babel/types");
+const mapAccumArray = require("@climb/map-accum");
+
+module.exports = function mapAccumNode({ concat, identity }, fOrDefinitions)
+{
+    const f = typeof fOrDefinitions === "object" ?
+        fromDefinitions(fOrDefinitions) : fOrDefinitions;
+
+    return function mapAccumNode(node)
+    {
+        if (node === null || node === void(0))
+            return [identity, node];
+
+        if (isArray(node))
+        {
+            const [accumOut, mapped] = mapAccumArray(
+                (accumIn, node) =>
+                    (([accumOut, mapped]) => [concat(accumIn, accumOut), mapped])
+                    (mapAccumNode(node)), identity, node);
+            const modified = mapped.some((mapped, index) => mapped !== node[index]);
+
+            return [accumOut, modified ? mapped : node];
+        }
+
+        return f(mapAccumNode, node);
+    }
+}
+
+module.exports.fromDefinitions = function fromDefinitions(definitions)
+{
+    return function f(mapAccumNode, node)
+    {
+        if (definitions[node.type])
+            return definitions[node.type](mapAccumNode, node);
+
+        const fields = t.VISITOR_KEYS[node.type];
+        const children = fields.map(field => node[field]);
+        const [accumOut, mapped] = mapAccumNode(children);
+        const modified = children
+            .map((child, index) => [child, index, fields[index]])
+            .filter(([child, index]) => child !== mapped[index]);
+        const newNode = modified.length === 0 ?
+            node :
+            modified.reduce((accum, [, index, field]) =>
+                (accum[field] = mapped[index], accum), { ...node });
+
+        return [accumOut, newNode];
+    }
+}
---
+++ to-lambda-form/transform-scope.js
@@ -0,0 +1,120 @@
+const Scope = require("./scope");
+
+const ifChanged = (node, changes) =>
+    Object.keys(changes).some(field => node[field] !== changes[field]) ?
+        { ...node, ...changes } : node;
+
+
+module.exports = require("./map-accum-node").fromDefinitions(
+{
+    Identifier(mapAccumNode, node)
+    {
+        return [Scope.fromFree(node.name), node];
+    },
+
+    VariableDeclarator(mapAccumNode, node)
+    {
+        const [initScope, init] = mapAccumNode(node.init);
+        const [idScope, id] = fromPattern(mapAccumNode, node.id);
+        const mapped = ifChanged(node, { init, id });
+        const scope = Scope.concat(idScope, initScope);
+
+        return [scope, mapped];
+    },
+
+    FunctionDeclaration(mapAccumNode, node)
+    {
+        const [bodyScope, body] = mapAccumNode(node.body);
+        const [idScope, id] = fromPattern(node.id);
+        const [parametersScope, nodeWithModifiedParameters] =
+            fromArrayFieldPattern(mapAccumNode, node, "params");
+        const scope = [bodyScope, idScope, parametersScope]
+            .reduce(Scope.concat, Scope.identity);
+        const mapped = ifChanged(nodeWithModifiedParameters, { body, id });
+
+        return [scope, mapped];
+    },
+
+    FunctionExpression,
+    ArrowFunctionExpression: FunctionExpression,
+ 
+    BlockStatement(mapAccumNode, node)
+    {
+        const [bodyScope, body] = mapAccumNode(node.body);
+        const mapped = ifChanged(node, { body });
+        const scope = Scope({ free: bodyScope.free });
+
+        return [scope, mapped];
+    }
+});
+
+function FunctionExpression(mapAccumNode, node)
+{
+    const [bodyScope, body] = mapAccumNode(node.body);
+    const [idScope, id] = fromPattern(node.id);
+    const [parametersScope, nodeWithModifiedParameters] =
+        fromArrayFieldPattern(mapAccumNode, node, "params");
+    const scopeWithIdScope = [bodyScope, idScope, parametersScope]
+        .reduce(Scope.concat, Scope.identity);
+    const scope = !id ? 
+        scopeWithIdScope :
+        Scope({ ...scopeWithIdScope, bound: scopeWithIdScope.bound.remove(id.name) });
+    const mapped = ifChanged(nodeWithModifiedParameters, { body, id });
+
+    return [scope, mapped];
+}
+    
+function fromPattern(mapAccumNode, pattern)
+{
+    if (!pattern)
+        return [Scope.identity, pattern];
+
+    const type = pattern.type;
+
+    if (type === "AssignmentPattern")
+    {
+        const [leftScope, left] = fromPattern(mapAccumNode, pattern);
+        const [rightScope, right] = mapAccumNode(pattern);
+        const scope = Scope.concat(leftScope, rightScope);
+        const mapped = ifChanged(pattern, { left, right });
+
+        return [scope, mapped];
+    }
+
+    if (type === "ArrayPattern")
+        return fromArrayFieldPattern(mapAccumNode, pattern, "elements");
+
+    if (type === "ArrayPattern")
+        return fromArrayFieldPattern(mapAccumNode, pattern, "properties");
+
+    if (type === "ObjectProperty")
+    {
+        const [keyScope, key] = fromPattern(mapAccumNode, pattern.key);
+        const [valueScope, value] = mapAccumNode(pattern.value);
+        const mapped = ifChanged(pattern, { key, value });
+
+        return [Scope.concat(keyScope, valueScope), mapped];
+    }
+
+    if (type === "RestElement")
+        return fromPattern(mapAccumNode, pattern.argument);
+
+    if (type === "Identifier")
+        return [Scope.fromBound(pattern.name), pattern];
+
+    throw Error("Unreachable");
+}
+
+function fromArrayFieldPattern(mapAccumNode, node, field)
+{
+    const pairs = node[field].map(child => fromPattern(mapAccumNode, child));
+    const scope = pairs.reduce((scope, [childScope]) =>
+        Scope.concat(scope, childScope),
+        Scope.identity);
+    const children = pairs.map(([, child]) => child);
+    const modified = node[field]
+        .some((original, index) => original !== children[index]);
+    const mapped = modified ? { ...node, [field]: children } : node;
+
+    return [scope, mapped];
+}

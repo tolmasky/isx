96a505c742b3146c1ca0205dcd093c19395ce4af
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-06-09 20:36:40 -0700

    Glob in local and docker.
    Reviewed by @tolmasky.

---
+++ glob.js
@@ -0,0 +1,117 @@
+const { is, string } = require("@algebraic/type");
+const { stdout: spawn } = require("@cause/task/spawn");
+const toPooled = require("@cause/task/transform/to-pooled");
+
+const find = (...args) => spawn("find", args);
+const findInDocker = (tag, ...args) =>
+    spawn("docker", ["run", "--rm", tag, "find", "/", ...args]);
+
+
+module.exports = toPooled(["command"], function glob(source, patterns)
+{
+    const local = is(string, source);
+
+    // FIXME: We should do "-o -empty -type d" with the same pattern without the
+    // trailing piece.
+    const optional = local ? "\\{0,1\\}" : "?";
+    const globToRegExp = glob =>
+        (local ? `${source}/${glob}` : `/${glob}`)
+            .replace(/[\?\.]/g, character => `\\${character}`)
+            .replace(/\*/g, "[^\/]*") + `\\(/.*\\)${optional}`;
+
+    const command = local ? find : findInDocker;
+    const output = command(
+        local ? source : source.identifier,
+        "-type", "f",
+        ...patterns
+            .map(globToRegExp)
+            .flatMap((pattern, index) =>
+                [...(index > 0 ? ["-o"] : []), "-regex", pattern]));
+    const filenames = [...output.matchAll(/([^\n]*)\n/g)]
+        .map(([_, filename]) => filename);
+
+    return filenames;
+}, { find, findInDocker, is, string });
+
+const toPromise = require("@cause/cause/to-promise");
+
+(async function ()
+{
+const glob = module.exports;
+try {
+    console.log("what?...");
+    console.log(await toPromise(Object, glob("/Users/tolmasky/Development/tonic", ["app/build"])));
+    console.log(await toPromise(Object, glob({ identifier: "aa0705969327" }, ["node-v10.15.3-linux-x64.tar.xz"])));
+}
+catch(e)
+{
+    console.log(e);
+}
+
+})();
+
+/*
+
+const { fromAsync } = require("@cause/task");
+const { stdout: spawn } = require("@cause/task/spawn");
+
+const command = (binary, ...prefix) =>
+    (...args) => spawn(binary, [...prefix, ...args]);
+
+const darwin = require("os").platform() === "darwin";
+const tar = command(darwin ? "gtar" : "tar");
+const shasum = command(...(darwin ?
+    ["shasum", "-a", "256"] : ["sha256sum"]));
+    
+    
+function glob(source, patterns)
+{
+    const names = patterns.flatMap((pattern, index) =>
+        [...(index > 0 ? ["-o"] : []), "-name", pattern]);
+    const arguments = ["-print0", ...names];
+
+    if (is(string, source))
+        return spawn("find", [".", ...arguments], { cwd: source });
+
+    return spawn("docker", ["run", source.identifier, cwd + " " + arguments.join(" ")]) 
+}
+
+function toFindArguments(patterns)
+{
+    return 
+}
+
+
+data `Source` (
+    from    => Optional(string),
+    pattern => string );
+
+const Patterns = Map(Optional(string), List(string));
+
+
+function (patternGroups)
+{
+    map(patternGroups)
+}
+
+function (image, patterns)
+{
+    // If this is on the local filesystem, we can immediately just grab matching
+    // filenames.
+    if (image === None)
+        return getFilenames(patterns);
+
+    // If not, then we have to first find all the paths that match in the image
+}
+
+function glob(image, patterns)
+{
+
+}
+
+
+const fullGlob = toPooled("glob", function fullGlob(glob)
+{
+    
+}
+*/
